5a2951dd75bc2f1e2ff09079aa1fc3365505e137
monaalaa <106662972+MonaBindary1@users.noreply.github.com>
2023-02-19 21:04:51 +0100

    create user document woth firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,12 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utlis";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utlis";
+
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
   return (
     <div>
