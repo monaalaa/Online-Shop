4ee5d14a403ef37dab37a8018b8d7bce108eaf79
monaalaa <106662972+MonaBindary1@users.noreply.github.com>
2023-02-20 10:11:36 +0100

    try to sign In Wit hGoogle Redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,39 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utlis";
 
 const SignIn = () => {
+  useEffect(() => {
+    const fetch = () => {
+      //const response = await getRedirectResult(auth);
+      console.log(" ************");
+    };
+    fetch();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signInWithGoogleRedirect();
+    console.log(user);
+  };
+
   return (
     <div>
       <h1> Sign-In page</h1>
       <button onClick={logGoogleUser}>sign in with google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        sign in with google redirect
+      </button>
     </div>
   );
 };
