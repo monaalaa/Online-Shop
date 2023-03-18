73058ae2b061c0a9fb3d1ad0c8b1f1a512c34c2e
monaalaa <monabindary@gmail.com>
2023-03-18 09:55:03 +0100

    get products data from firebase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,12 +1,24 @@
-import { createContext, useState } from "react";
-import PRODUCTS from "../shop-data.json";
-
+import { createContext, useState, useEffect } from "react";
+//import SHOP_DATA from "../shop-data.js";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utlis.js";
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+  /* 
+  console.log(SHOP_DATA);
+  useEffect(() => {
+    addCollectionAndDocuments("data", SHOP_DATA);
+  }, []);*/
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -14,7 +14,6 @@ export const UserProvider = ({ children }) => {
   const value = { currentUser, setCurrentUser };
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-      console.log(user);
       if (user) {
         createUserDocumentFromAuth(user);
       }
