d2ddbb2da5243ef3959131838fc35ceadfdbf413
monaalaa <monabindary@gmail.com>
2023-03-30 14:33:29 +0200

    create category componets

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,16 +1,17 @@
 import { createContext, useState, useEffect } from "react";
 //import SHOP_DATA from "../shop-data.js";
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utlis.js";
-export const ProductsContext = createContext({
-  products: [],
+export const CategoriesContext = createContext({
+  categoriesMap: {},
 });
 
-export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
       console.log(categoryMap);
+      setCategoriesMap(categoryMap);
     };
     getCategoriesMap();
   }, []);
@@ -19,10 +20,10 @@ export const ProductsProvider = ({ children }) => {
   useEffect(() => {
     addCollectionAndDocuments("data", SHOP_DATA);
   }, []);*/
-  const value = { products };
+  const value = { categoriesMap };
   return (
-    <ProductsContext.Provider value={value}>
+    <CategoriesContext.Provider value={value}>
       {children}
-    </ProductsContext.Provider>
+    </CategoriesContext.Provider>
   );
 };
